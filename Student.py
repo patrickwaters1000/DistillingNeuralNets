106574ab3029de057d57d7114f57773ce53155b1
Patrick Waters <patrick.waters1000@gmail.com>
2018-06-22 16:09:13 -0700

    Updated Student.py and Teacher.py

--- Student.py
+++ Student.py
@@ -5,23 +5,22 @@ Then build the top model, which can be trained from:
 the teacher's logits using the -s option (student),
 or the true labels using the -d option (delinquent)
 '''
-import os
-import keras
-import sys
+import os, sys, re, pickle, keras
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dropout, Flatten, Dense, Input, Softmax
 from keras import applications
-import re
-import pickle
 
 options = sys.argv[1:]
-class_names = ["owl","pigeon"]
-class_name_indices = {"owl":0, "pigeon":1}
+class_names = os.listdir("data/train")
+class_name_indices = {name:i for i,name in enumerate(class_names)}
+nbr_classes = len(class_names)
 
 if "-f" in options:
-    def get_features(img_path):
+# Calculate the MobileNet's convolution output for each sample and store it.
+# This makes transfer learning much faster.
+    def get_features(img_path): 
         img = keras.preprocessing.image.load_img(img_path,target_size=(128,128))
         img = keras.preprocessing.image.img_to_array(img)
         img = keras.applications.mobilenet.preprocess_input(np.array([img]))
@@ -39,116 +38,79 @@ if "-f" in options:
             sample["student_feature_path"]=feature_path
         pickle.dump(df,open("{}_data.p".format(case),"wb"))
 
-if "-s" in options:
-    def train_generator(df,batch_size,shuffle=True):
-        feature_paths = np.array([s["student_feature_path"] for s in df])
-        teacher_logits = np.array([s["teacher_logits"] for s in df])
-        nbr_samples = len(feature_paths)
-        nbr_batches = nbr_samples // batch_size
-        print("The number of samples is {}".format(nbr_samples))
-        while True:
-            perm = np.arange(nbr_samples)
-            if shuffle:
-                np.random.shuffle(perm)
-            
-            for i in range(nbr_batches):
-                indices = perm[i*batch_size:(i+1)*batch_size]
-                batch_feature_paths = feature_paths[indices]
-                batch_logits = teacher_logits[indices]
-                batch_features = np.array([np.load(p) for p in batch_feature_paths])
-                yield (batch_features,batch_logits)
 
-    df = pickle.load(open("train_data.p","rb"))
-    g = train_generator(df,32)
+def get_generator(df,y_name,batch_size,shuffle=True):
+# This generates batches of training or validation data for training the student.    
+    X = np.array([s["student_feature_path"] for s in df])
+    Y = np.array([s[y_name] for s in df])
+    nbr_samples = len(X)
+    nbr_batches = nbr_samples // batch_size
+        
+    while True:
+        perm = np.arange(nbr_samples)
+        if shuffle:
+            np.random.shuffle(perm)
+            
+        for i in range(nbr_batches):
+            indices = perm[i*batch_size:(i+1)*batch_size]
+            X_batch = X[indices]
+            Y_batch = Y[indices]
+            X_batch = np.array([np.load(p) for p in X_batch])
+            if y_name=="label":
+                Y_batch = keras.utils.to_categorical(Y_batch,nbr_classes)
+            yield (X_batch,Y_batch)
+
+if "-s" in options:
+# s stands for student (as opposed to train delinquent).  
+# This block trains the student using stored convolution features   
+    df_train = pickle.load(open("train_data.p","rb"))
+    df_test = pickle.load(open("test_data.p","rb"))
+    g_train = get_generator(df_train,"teacher_logits",32)
+    g_test = get_generator(df_test,"label",32,shuffle=False)
     
     top_model = keras.models.Sequential()
     top_model.add(Flatten(input_shape=(4,4,256)))
     top_model.add(Dense(200,activation="relu"))
     top_model.add(Dropout(0.3))
-    top_model.add(Dense(2,activation=None,name="logits"))
+    top_model.add(Dense(nbr_classes,activation=None,name="logits"))
     top_model.add(Softmax())
+    top_model.compile(
+        loss=keras.losses.categorical_crossentropy,
+        optimizer=keras.optimizers.Adam(),
+        metrics=["acc"])
     logits_model = keras.models.Model(inputs=top_model.layers[0].input,outputs=top_model.get_layer("logits").output)
     logits_model.compile(
         loss=keras.losses.mean_squared_error,
-        optimizer = keras.optimizers.Adam())
-    logits_model.fit_generator(g,steps_per_epoch=100,epochs=100)
+        optimizer = keras.optimizers.Adam(lr=1e-5))
+    
+    log = []
+    for epoch in range(300):
+        logits_model.fit_generator(g_train,steps_per_epoch=len(df_train)//32,epochs=1,verbose=0)
+        how_good = top_model.evaluate_generator(g_test,steps=len(df_test)//32)
+        print("Epoch {} validation results are {}".format(epoch,how_good))
+        log.append(how_good)
+    pickle.dump(log,open("stats/student_train_log.p","wb"))
     top_model.save("models/student.h5")
 
-
-if "-e" in options:
-     
-    student_convnet = keras.applications.MobileNet(input_shape=(128,128,3),alpha=0.25,include_top=False,weights="imagenet")
-    top_model = keras.models.load_model("models/student.h5")
-    inp = Input(shape=(128,128,3))
-    x = student_convnet(inp)
-    out = top_model(x)
-    full_model = keras.models.Model(inputs=inp,outputs=out)
-    full_model.compile(optimizer=keras.optimizers.Adam(),loss=keras.losses.categorical_crossentropy,metrics=["acc"])
-
-    def preprocess(img_path):
-        img = keras.preprocessing.image.load_img(img_path,target_size=(128,128))
-        img = keras.preprocessing.image.img_to_array(img)
-        img = keras.applications.mobilenet.preprocess_input(np.array([img]))
-        return img[0]
-
-    def test_generator(df,batch_size,shuffle=True):
-        image_paths = np.array([s["image_path"] for s in df])
-        labels = keras.utils.to_categorical(np.array([s["label"] for s in df]))
-        nbr_samples = len(image_paths)
-        nbr_batches = nbr_samples // batch_size
-
-        while True:
-            perm = np.arange(nbr_samples)
-            if shuffle:
-                np.random.shuffle(perm)
-            
-            for i in range(nbr_batches):
-                indices = perm[i*batch_size:(i+1)*batch_size]
-                batch_img_paths = image_paths[indices]
-                batch_images = np.array([preprocess(p) for p in batch_img_paths])
-                batch_labels = labels[indices]
-                yield (batch_images,batch_labels)
-
-    df = pickle.load(open("test_data.p","rb"))
-    g = test_generator(df,32,shuffle=True)
-    how_good=full_model.evaluate_generator(g,steps=20)
-    print("Here are the results from evaluating the student model-- {}".format(how_good))
-
 if "-d" in options:
-    def get_generator(df,batch_size,shuffle=True):
-        feature_paths = np.array([s["student_feature_path"] for s in df])
-        labels = np.array([s["label"] for s in df])
-        labels = keras.utils.to_categorical(labels)
-        nbr_samples = len(feature_paths)
-        nbr_batches = nbr_samples // batch_size
-        while True:
-            perm = np.arange(nbr_samples)
-            if shuffle:
-                np.random.shuffle(perm)
-            
-            for i in range(nbr_batches):
-                indices = perm[i*batch_size:(i+1)*batch_size]
-                batch_feature_paths = feature_paths[indices]
-                batch_labels = labels[indices]
-                batch_features = np.array([np.load(p) for p in batch_feature_paths])
-                yield (batch_features,batch_labels)
-
+# Train a delinquent model (one that learns from the ground truth labels, ignoring the teacher)
+# Used for benchmarking the performance of the student.
     df_train = pickle.load(open("train_data.p","rb"))
     df_test = pickle.load(open("test_data.p","rb"))
-    g_train = get_generator(df_train,32)
-    g_test = get_generator(df_test,32)
+    g_train = get_generator(df_train,"label",32)
+    g_test = get_generator(df_test,"label",32,shuffle=False)
     
     top_model = keras.models.Sequential()
     top_model.add(Flatten(input_shape=(4,4,256)))
     top_model.add(Dense(200,activation="relu"))
     top_model.add(Dropout(0.3))
-    top_model.add(Dense(2,activation=None,name="logits"))
+    top_model.add(Dense(nbr_classes,activation=None,name="logits"))
     top_model.add(Softmax())
     
     top_model.compile(
         loss=keras.losses.categorical_crossentropy,
-        optimizer = keras.optimizers.Adam(),
+        optimizer = keras.optimizers.Adam(lr=1e-5),
         metrics=["acc"])
-    top_model.fit_generator(g_train,steps_per_epoch=100,validation_data=g_test,validation_steps=10,epochs=10)
-
+    results = top_model.fit_generator(g_train,steps_per_epoch=len(df_train)//32,validation_data=g_test,validation_steps=len(df_test)//32,epochs=150)
+    pickle.dump(results.history["val_acc"],open("stats/delinquent_val_acc.p","wb"))
 
--- Teacher.py
+++ Teacher.py
@@ -1,107 +1,125 @@
 '''
-In this script we retrain a given layer or layers
-with other layers frozen.
+In this script we build the teacher model used in distillation,
+and also use it to generate the rich labels.
 '''
-import os
-import keras
-import sys
 import numpy as np
-from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dropout, Flatten, Dense, Input, Softmax
 from keras import applications
-import re
-import pickle
+import re, os, sys, pickle, keras
 
 options = sys.argv[1:]
-class_names = ["owl","pigeon"]
-class_name_indices = {"owl":0, "pigeon":1}
+class_names = os.listdir("data/train") # Train should have a subfolder for each class like "cat", "dog", etc.
+class_name_indices = {name:i for i,name in enumerate(class_names)}
 img_width, img_height = 224, 224
 
-if "-f" in options:
-    conv_model = applications.InceptionV3(include_top=False, weights='imagenet')
-    for case in ["train","test"]:
+if "-d" in options: 
+# Build a database containing the location of each image, and the image's label
+# The database will be a list of dictionaries
+# Separate databases for train and test
+    for case in ["train","test"]: 
         database = []
         for class_name in class_names:
-            print("Calculating features for {} samples of class {}".format(case,class_name))
             source_folder = "data/{}/{}".format(case,class_name)
-            target_folder = "features/teacher/{}/{}".format(case,class_name)
             for filename in os.listdir(source_folder):
-                new_filename = re.sub(r".jpg$",".npy",filename)
                 img_path="{}/{}".format(source_folder,filename)
-                img =keras.preprocessing.image.load_img(img_path,target_size=(img_width,img_height))
-                img = keras.preprocessing.image.img_to_array(img)
-                img = keras.applications.inception_v3.preprocess_input(np.array([img]))
-                features = conv_model.predict(img)[0]
-                feature_path ="{}/{}".format(target_folder,new_filename)
-                np.save(feature_path,features)
                 database.append({
                     "image_path":img_path,
                     "label":class_name_indices[class_name],
                     "label_name":class_name,
-                    "teacher_feature_path":feature_path})
         pickle.dump(database,open("{}_data.p".format(case),"wb"))
 
 
-def batch_generator(folder,batch_size):
+if "-f" in options:
+# Calculate output of convolutional layers for each sample, and saves them so that transfer learning can be done quickly
+    def preprocess(p):  #preprocess the image stored at path p
+        img = keras.preprocessing.image.load_img(img_path,target_size=(img_width,img_height))
+        img = keras.preprocessing.image.img_to_array(img)
+        img = keras.applications.inception_v3.preprocess_input(np.array([img]))
+        return img[0]
+
+    conv_model = applications.InceptionV3(include_top=False, weights='imagenet')
+    for case in ["train","test"]:
+        df = pickle.load(open("{}_data.p".format(case),"rb"))
+	for sample in df:
+            name = sample["label_name"]
+            path = sample["image_path"]
+            img = preprocess(path)
+            features = conv_model.predict(img.reshape(1,*img.shape))[0]
+            new_path = re.sub("data","features/teacher",path)
+            new_path = re.sub(r".jpg$",".npy",path)
+            sample["teacher_feature_path"]=new_path
+            np.save(new_path,features)
+        pickle.dump(df,open("{}_data.p".format(case),"wb"))
+    
+   
+
+def batch_generator(df,batch_size,shuffle=True):
+# Generates train and validation batches for training the teacher model
+    X = np.array([s["teacher_feature_path"] for s in df])
+    Y = np.array([s["label"] for s in df])
+    nbr_samples = len(X)
+    nbr_batches = nbr_samples // batch_size
+    
     while True:
-        features = np.empty((batch_size,5,5,2048))
-        labels = np.empty((batch_size,))
-        for i in range(batch_size):
-            random_label = np.random.randint(len(class_names))
-            class_name = class_names[random_label]
-            filenames = os.listdir("{}/{}".format(folder,class_name))
-            random_filename = np.random.choice(filenames)
-            sample = np.load("{}/{}/{}".format(folder,class_name,random_filename))
-            features[i] = sample
-            labels[i] = random_label
-        labels = keras.utils.to_categorical(labels)
-        yield (features,labels)
+        perm = np.arange(nbr_samples)
+        if shuffle:
+            np.random.shuffle(perm)
+        
+        for i in range(nbr_batches):
+            indices = perm[i*batch_size:(i+1)*batch_size]
+            X_batch = X[indices]
+            Y_batch = Y[indices]
+            X_batch = np.array([np.load(p) for p in X_batch])
+            Y_batch = keras.utils.to_categorical(Y_batch,num_classes=5)
+            yield (X_batch,Y_batch)
 
 
 
 if "-t" in options:
+    df_train = pickle.load(open("train_data.p","rb"))
+    df_test = pickle.load(open("test_data.p","rb"))
+    nbr_train = len(df_train)
+    nbr_test = len(df_test)
+
     feature_shape=(5,5,2048)
-    train_generator = batch_generator("features/teacher/train",32)
-    test_generator = batch_generator("features/teacher/test",32)
+    train_generator = batch_generator(df_train,32)
+    test_generator = batch_generator(df_test,32,shuffle=False)
     top_model = Sequential()
     top_model.add(Flatten(input_shape=(5,5,2048)))
     top_model.add(Dense(256, activation='relu',kernel_regularizer=keras.regularizers.l2(0.0001)))
     top_model.add(Dropout(0.5))
-    top_model.add(Dense(2, activation=None,kernel_regularizer=keras.regularizers.l2(0.0001),name="logits"))
+    top_model.add(Dense(len(class_names), activation=None,kernel_regularizer=keras.regularizers.l2(0.0001),name="logits"))
     top_model.add(Softmax())
-    top_model.compile(optimizer=keras.optimizers.RMSprop(),
-                    loss='categorical_crossentropy',
-                    metrics=['accuracy'])
-    top_model.fit_generator(train_generator,steps_per_epoch=100,validation_data=test_generator,validation_steps=10,epochs=10)   
+    top_model.compile(
+        optimizer=keras.optimizers.RMSprop(lr=1e-6),
+        loss='categorical_crossentropy',
+        metrics=['accuracy'])
+    ckpt = keras.callbacks.ModelCheckpoint("models/teacher_epoch{epoch:03d}.h5")
+    h = top_model.fit_generator(
+        train_generator,
+        steps_per_epoch=nbr_train // 32,
+        validation_data=test_generator,
+        validation_steps=nbr_test // 32,
+        epochs=40,
+        callbacks=[ckpt])
+    print("The keys are {}".format(h.history.keys()))
+    pickle.dump(h.history["val_acc"],open("stats/teacher_val_acc.p","wb"))
     top_model.save("models/teacher_top_model.h5")
 
 
-
-
-def nonrandom_batch_generator(folder,batch_size):
-    
-    paths_by_label = [ ["{}/{}/{}".format(folder,name,x) for x in os.listdir("{}/{}".format(folder,name))] for name in class_names]
-    print(paths_by_label)
-    paths,labels = [],[]
-    for i,group in enumerate(paths_by_label):
-        paths+=group
-        labels+=[i for p in group]
-    nbr_samples = len(paths)
-
-    q,r = divmod(nbr_samples,batch_size)
-    nbr_batches = q +(1 if r>0 else 0)
-    for i in range(nbr_batches):
-        batch_samples = [np.load(p) for p in paths[i*batch_size:(i+1)*batch_size]]
-        batch_labels = labels[i*batch_size:(i+1)*batch_size]
-        yield (np.array(batch_samples),np.array(batch_labels))
-                
-if "-l" in options: # l for logits
+               
+if "-l" in options:
+# Calculate the rich labels,
+# which for now are the teacher model's logits (hidden state before final softmax)
+# Saves the rich labels so that the student can be trained with them.
     top_model = keras.models.load_model("models/teacher_top_model.h5")
-    logits_model = keras.models.Model(inputs=top_model.layers[0].input,outputs=top_model.get_layer("logits").output)
+    logits_model = keras.models.Model( # build a model that grabs the hidden state just before final softmax
+        inputs=top_model.layers[0].input,
+        outputs=top_model.get_layer("logits").output)
 
     df = pickle.load(open("train_data.p","rb"))
-    for sample in df:
+    for sample in df: # calculate logits for each training sample
         feature_path = sample["teacher_feature_path"]
         features = np.load(feature_path)
         features = np.array([features])
